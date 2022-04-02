e2efe6362702d60154c16242a6ece9b794209097
L-j-h-c <ckrgkswnsgh@naver.com>
2022-04-02 15:05:50 +0900

    [Add] #1 - 과제 프로젝트 생성
    과제 프로젝트 생성

---
+++ Seminar/30th-SOPT-iOS-assignment/30th-SOPT-iOS-assignment/30th-SOPT-iOS-assignment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  30th-SOPT-iOS-assignment
+//
+//  Created by Junho Lee on 2022/04/02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
