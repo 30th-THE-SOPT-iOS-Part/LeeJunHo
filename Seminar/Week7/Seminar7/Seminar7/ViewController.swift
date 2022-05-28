92afc44e4d95c15322b792c71005cc13345baed2
L-j-h-c <ckrgkswnsgh@naver.com>
2022-05-28 14:28:07 +0900

    [Add] #21 - 프로젝트 생성

---
+++ Seminar/Week7/Seminar7/Seminar7/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Seminar7
+//
+//  Created by Junho Lee on 2022/05/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
